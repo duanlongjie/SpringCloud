5d47ad1a84a7f8847db8810db45c62da287de4c7
duanlongjie <2663824060@qq.com>
2022-03-01 22:40:27 +0800

    eureka集群、ribbon、feign、hystrix

--- consumer/src/main/java/consumer/ConsumerApplication.java
+++ consumer/src/main/java/consumer/ConsumerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(basePackages = "consumer.service")
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableZuulProxy
 //@EnableTurbine
 public class ConsumerApplication {
     public static void main(String[] args) {
